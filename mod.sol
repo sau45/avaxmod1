f4e60cd9928845698f2fbe1aca2f71f45b89e72e
Saurav Bharti <87286258+sau45@users.noreply.github.com>
2024-02-25 01:10:43 +0530

    Update mod.sol

--- mod.sol
+++ mod.sol
@@ -1,1 +1,72 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SupplyChain {
+    address public owner;
+    enum OrderStatus { Pending , InProgress , Shipped , Delivered }
+
+    struct Order{
+        address buyer;
+        string product ; 
+        uint256 quantity;
+        OrderStatus status ; 
+    }
+
+    mapping(uint256 => Order) public orders;
+    uint256 public orderCount;
+
+    event OrderPlaced(uint256 orderId , address buyer , string product , uint256 quantity );
+    event OrderProcessed(uint256 orderId);
+    event OrderShipped (uint256 orderId);
+    event OrderDelivered (uint256 orderId);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+        orderCount = 0 ; 
+    }
+
+    function placeOrder( string memory _product , uint256 _quantity) external {
+        // Use require() to ensure the campaign is ongoing
+        require(_quantity > 0 , "Quantity must be greater than zero");
+        uint256 orderId = orderCount++;
+        orders[orderId] = Order( msg.sender , _product , _quantity , OrderStatus.Pending);
+
+
+        emit OrderPlaced(orderId , msg.sender , _product , _quantity);
+
+    }
+
+    function processOrder( uint256 _orderId ) external onlyOwner {
+        Order storage order = orders[_orderId];
+       require(order.status == OrderStatus.Pending , "Order is not pending");
+        
+        uint256 newQuantity = order.quantity + 1 ;
+        assert(newQuantity  > order.quantity);
+
+        order.quantity = newQuantity;
+        order.status =  OrderStatus.InProgress;
+
+       emit OrderProcessed(_orderId);
+    }
+
+    function shipOrder( uint256 _orderId ) external  onlyOwner{
+        Order storage order = orders[_orderId];
+        assert(order.status == OrderStatus.Shipped);
+        emit OrderShipped(_orderId);
+    }
+
+    function deliverOrder( uint256 _orderId ) external  onlyOwner{
+        Order storage order = orders[_orderId];
+        if( order.status != OrderStatus.Shipped){
+            revert("Order is not shipped");
+        }
+        order.status = OrderStatus.Delivered;
+        emit OrderDelivered(_orderId);
+    }
+}
